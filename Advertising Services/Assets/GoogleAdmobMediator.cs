a9786ea00982dd2316666808267c71e23286884f
jackcarroll5 <jackcarroll9000@gmail.com>
2020-03-14 21:20:52 +0000

    Added Rewarded Ad AdMob video button with functions.

--- Advertising Services/Assets/GoogleAdmobMediator.cs
+++ Advertising Services/Assets/GoogleAdmobMediator.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
 using System;
+using UnityEngine.UI;
 
 public class GoogleAdmobMediator : MonoBehaviour
 {
+    private InterstitialAd interstitial;
     public BannerView bannerView;
+    private RewardedAd rewardedAd;
+
     // Start is called before the first frame update
-   public void Start()
+    public void Start()
     {
         string appId = "ca-app-pub-3940256099942544~3347511713";
 
         MobileAds.Initialize(appId);
 
         RequestBanner();
+        RequestInterstitial();
     }
+ void OnGUI()
+    {
+        if (GUI.Button(new Rect(200, 200, Screen.width / 6, Screen.height / 8), "Test"))
+            ShowInterstitial();
+    }
+
     public void HandleOnAdLoaded(object sender, EventArgs args)
     {
         MonoBehaviour.print("HandleAdLoaded event received");
@@ -34,12 +45,75 @@ public class GoogleAdmobMediator : MonoBehaviour
 
     public void HandleOnAdClosed(object sender, EventArgs args)
     {
-        MonoBehaviour.print("HandleAdClosed event received");
+        RequestInterstitial();
     }
 
     public void HandleOnAdLeavingApplication(object sender, EventArgs args)
     {
         MonoBehaviour.print("HandleAdLeavingApplication event received");
+    }
+
+    public void CreateLoadRewardedAd()
+    {
+        string adUnitId = "ca-app-pub-3940256099942544/5224354917";
+
+        this.rewardedAd = new RewardedAd(adUnitId);
+
+        // Called when an ad request has successfully loaded.
+        this.rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
+        // Called when an ad request failed to load.
+        this.rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
+        // Called when an ad is shown.
+        this.rewardedAd.OnAdOpening += HandleRewardedAdOpening;
+        // Called when an ad request failed to show.
+        this.rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
+        // Called when the user should be rewarded for interacting with the ad.
+        this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+        // Called when the ad is closed.
+        this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded ad with the request.
+        this.rewardedAd.LoadAd(request);
+    }
+
+    public void HandleUserEarnedReward(object sender, Reward args)
+    {
+        string type = args.Type;
+        double amount = args.Amount;
+        MonoBehaviour.print(
+            "HandleRewardedAdRewarded event received for "
+                        + amount.ToString() + " " + type);
+    }
+
+    public void HandleRewardedAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardedAdLoaded event received");
+    }
+
+    public void HandleRewardedAdFailedToLoad(object sender, AdErrorEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleRewardedAdFailedToLoad event received with message: "
+                             + args.Message);
+    }
+
+    public void HandleRewardedAdOpening(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardedAdOpening event received");
+    }
+
+    public void HandleRewardedAdFailedToShow(object sender, AdErrorEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleRewardedAdFailedToShow event received with message: "
+                             + args.Message);
+    }
+
+    public void HandleRewardedAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardedAdClosed event received");
     }
 
     public void RequestBanner()
@@ -64,9 +138,38 @@ public class GoogleAdmobMediator : MonoBehaviour
 
         // Load the banner with the request.
         this.bannerView.LoadAd(request);
-
     }
 
+    private void RequestInterstitial()
+    {
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+
+        // Called when an ad request has successfully loaded.
+        this.interstitial.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is shown.
+        this.interstitial.OnAdOpening += HandleOnAdOpened;
+        // Called when the ad is closed.
+        this.interstitial.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.interstitial.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        this.interstitial.LoadAd(request);
+    }
+
+    public void ShowInterstitial()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
+    }
 
     // Update is called once per frame
     void Update()
