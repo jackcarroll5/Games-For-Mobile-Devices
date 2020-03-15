20bb6de6ad504d2b774744d7d9eafee0efc98ce0
jackcarroll5 <jackcarroll9000@gmail.com>
2020-03-15 21:46:20 +0000

    Rewarded Ad loads but issue to launch ad on click to solve out

--- Advertising Services/Assets/GoogleAdmobMediator.cs
+++ Advertising Services/Assets/GoogleAdmobMediator.cs
@@ -7,9 +7,13 @@ using UnityEngine.UI;
 
 public class GoogleAdmobMediator : MonoBehaviour
 {
+    Button button;
     private InterstitialAd interstitial;
     public BannerView bannerView;
     private RewardedAd rewardedAd;
+    public Text rewardText;
+    private int amountGame = 0;
+    private int amountReward = 0;
 
     // Start is called before the first frame update
     public void Start()
@@ -20,27 +24,12 @@ public class GoogleAdmobMediator : MonoBehaviour
 
         RequestBanner();
         RequestInterstitial();
-    }
- void OnGUI()
-    {
-        if (GUI.Button(new Rect(200, 200, Screen.width / 6, Screen.height / 8), "Test"))
-            ShowInterstitial();
+        RequestLoadRewardedAd();
     }
 
     public void HandleOnAdLoaded(object sender, EventArgs args)
     {
-        MonoBehaviour.print("HandleAdLoaded event received");
-    }
-
-    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
-    {
-        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
-                            + args.Message);
-    }
-
-    public void HandleOnAdOpened(object sender, EventArgs args)
-    {
-        MonoBehaviour.print("HandleAdOpened event received");
+       
     }
 
     public void HandleOnAdClosed(object sender, EventArgs args)
@@ -48,12 +37,14 @@ public class GoogleAdmobMediator : MonoBehaviour
         RequestInterstitial();
     }
 
-    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    public void OnGetMorePointsClicked()
     {
-        MonoBehaviour.print("HandleAdLeavingApplication event received");
+        button.interactable = false;
+        button.GetComponentInChildren<Text>().text = "Loading...";
+        RequestLoadRewardedAd();
     }
 
-    public void CreateLoadRewardedAd()
+    private void RequestLoadRewardedAd()
     {
         string adUnitId = "ca-app-pub-3940256099942544/5224354917";
 
@@ -68,7 +59,8 @@ public class GoogleAdmobMediator : MonoBehaviour
         // Called when an ad request failed to show.
         this.rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
         // Called when the user should be rewarded for interacting with the ad.
-        this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+       // this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+        this.rewardedAd.OnUserEarnedReward += HandleOnAdRewarded;
         // Called when the ad is closed.
         this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
 
@@ -78,18 +70,57 @@ public class GoogleAdmobMediator : MonoBehaviour
         this.rewardedAd.LoadAd(request);
     }
 
-    public void HandleUserEarnedReward(object sender, Reward args)
+    void OnDestroy()
     {
-        string type = args.Type;
-        double amount = args.Amount;
-        MonoBehaviour.print(
-            "HandleRewardedAdRewarded event received for "
-                        + amount.ToString() + " " + type);
+        rewardedAd.OnAdLoaded -= this.HandleRewardedAdLoaded;
+        rewardedAd.OnUserEarnedReward -= this.HandleOnAdRewarded;
+        rewardedAd.OnAdClosed -= this.HandleRewardedAdClosed;
+    }
+
+    public void HandleOnAdRewarded(object sender, EventArgs args)
+    {//user finished watching ad
+        int points = int.Parse(rewardText.text);
+        points += 50; //add 50 points
+        rewardText.text = points.ToString();
     }
 
     public void HandleRewardedAdLoaded(object sender, EventArgs args)
     {
-        MonoBehaviour.print("HandleRewardedAdLoaded event received");
+        UserChoseToWatchAd();
+    }
+
+    private void HandleRewardedAdClosed(object sender, EventArgs args)
+    {
+        //RequestLoadRewardedAd();
+
+        button.GetComponentInChildren<Text>().text = "More Points";
+
+        rewardedAd.OnAdLoaded -= this.HandleRewardedAdLoaded;
+        rewardedAd.OnUserEarnedReward -= this.HandleOnAdRewarded;
+        rewardedAd.OnAdClosed -= this.HandleRewardedAdClosed;
+    }
+
+    private void HandleUserEarnedReward(object sender, Reward args)
+    {
+        double amount = args.Amount;
+        amountReward = (int)amount;
+    }
+
+    public void UserChoseToWatchAd()
+    {
+        if (rewardedAd.IsLoaded())
+            rewardedAd.Show();
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
     }
 
     public void HandleRewardedAdFailedToLoad(object sender, AdErrorEventArgs args)
@@ -111,9 +142,9 @@ public class GoogleAdmobMediator : MonoBehaviour
                              + args.Message);
     }
 
-    public void HandleRewardedAdClosed(object sender, EventArgs args)
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
     {
-        MonoBehaviour.print("HandleRewardedAdClosed event received");
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
     }
 
     public void RequestBanner()
@@ -128,10 +159,10 @@ public class GoogleAdmobMediator : MonoBehaviour
        this.bannerView.OnAdFailedToLoad += this.HandleOnAdFailedToLoad;
         ////// Called when an ad is clicked.
         this.bannerView.OnAdOpening += this.HandleOnAdOpened;
-        ////// Called when the user returned from the app after an ad click.
-        this.bannerView.OnAdClosed += this.HandleOnAdClosed;
         ////// Called when the ad click caused the user to leave the application.
        this.bannerView.OnAdLeavingApplication += this.HandleOnAdLeavingApplication;
+
+
 
         // Create an empty ad request.
         AdRequest request = new AdRequest.Builder().Build();
@@ -140,9 +171,12 @@ public class GoogleAdmobMediator : MonoBehaviour
         this.bannerView.LoadAd(request);
     }
 
-    private void RequestInterstitial()
+    public void RequestInterstitial()
     {
         string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+
+        if (this.interstitial != null)
+            this.interstitial.Destroy();
 
         // Initialize an InterstitialAd.
         this.interstitial = new InterstitialAd(adUnitId);
@@ -163,17 +197,20 @@ public class GoogleAdmobMediator : MonoBehaviour
         this.interstitial.LoadAd(request);
     }
 
-    public void ShowInterstitial()
+    public void ShowInterstitialAd()
     {
-        if (this.interstitial.IsLoaded())
-        {
-            this.interstitial.Show();
-        }
+        if (interstitial.IsLoaded())
+            interstitial.Show();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     if(amountReward > 0)
+        {
+            amountGame += amountReward;
+            rewardText.text = amountGame.ToString();
+            amountReward = 0;
+        }
     }
 }
